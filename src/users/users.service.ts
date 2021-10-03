e6e335bedb940778b4738056c5231b27e811a87f
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-03 13:11:19 -0300

    add user service remove func

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,5 +22,9 @@ export class UsersService {
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
-  remove() {}
+  async remove(id: number) {
+    const user = await this.repo.findOne(id);
+    if (!user) throw new Error('user not found');
+    return this.repo.remove(user);
+  }
 }
