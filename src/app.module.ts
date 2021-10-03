c134f2961f967605ea504c1bb969c418d5d94467
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-03 10:37:51 -0300

    fix app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UserController } from './user/user.controller';
-import { ReportsController } from './reports/reports.controller';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [UsersModule, ReportsModule],
-  controllers: [AppController, UserController, ReportsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
