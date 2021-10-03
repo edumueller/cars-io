401519fb8e48aeef7bfba22127d83bf6142af357
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-03 16:33:57 -0300

    add some type checking for serialize

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,8 +7,11 @@ import {
 import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-export function Serialize(dto: any) {
+interface ClassConstructor {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  new (...args: any[]): {};
+}
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 export class SerializeInterceptor implements NestInterceptor {
