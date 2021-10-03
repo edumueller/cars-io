71442a5b20e1f4532b322286e1401ca47c6994a7
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-03 13:15:01 -0300

    add get user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 @Controller('auth')
@@ -7,5 +7,9 @@ export class UsersController {
   @Post('signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
+  }
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
   }
 }
