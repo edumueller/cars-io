cd758915549f50a4fdfba48798111f749eb6d429
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-03 12:59:31 -0300

    chore: add comment about hooks

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,7 +7,7 @@ import { User } from './user.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
   create(email: string, password: string) {
-    const user = this.repo.create({ email, password });
+    const user = this.repo.create({ email, password }); // If you don't create using this function but save directly, hooks will not be executed
     return this.repo.save(user);
   }
 }
