01ac14162c9646897975a49d22dfb6d994cb3924
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-03 16:22:26 -0300

    chore: rm unused import

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -2,7 +2,6 @@ import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
