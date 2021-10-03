0c62141220cdc65daf74c4d6f684b248e880f4c1
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-03 10:36:44 -0300

    generate users mod, svc, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UserController } from './user/user.controller';
+import { ReportsController } from './reports/reports.controller';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, ReportsModule],
+  controllers: [AppController, UserController, ReportsController],
   providers: [AppService],
 })
 export class AppModule {}
