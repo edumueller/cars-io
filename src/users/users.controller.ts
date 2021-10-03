c14367286ee17a19506d061656c9fd543cc02c4d
Eduardo Nicastro <edu.nicastro@gmail.com>
2021-10-03 13:30:24 -0300

    add update user with given id route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
@@ -30,5 +31,10 @@ export class UsersController {
   @Delete('/:id')
   deleteUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
